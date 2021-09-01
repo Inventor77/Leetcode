733b46e2ad75a42f77fd204737eb04f36dc82e56
inventor77 <shreedutt77@gmail.com>
2021-09-01 22:23:55 +0530

    day1 sep 2021

---
+++ Challenges/Sep 2021/Sep_01_Array_Nesting.cpp
@@ -0,0 +1,76 @@
+//  -------------------------------  Bruteforce O(N^2) ---------------------------------------
+
+// class Solution {
+// public:
+//     int arrayNesting(vector<int>& nums) {
+//         int ans = 0;
+//         n = nums.size();
+//         for (int i = 0; i < n; i++) {
+//             unordered_set<int> u;
+//             while (!u.count(nums[i]))
+//             {
+//                 u.insert(nums[i]);
+//                 nums[i] = nums[nums[i]];
+//             }
+//             int s = u.size();
+//             ans = max(ans, s);
+//         }
+//         return ans;
+//     }
+// };
+
+//  ------------------------------------------------------------------------------------
+
+
+//     -----------  O(N^2) ----------------
+
+class Solution {
+public:
+    int arrayNesting(vector<int>& nums) {
+        int n = nums.size(), ans;
+        unordered_set<int> s;
+
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+
+            while (!s.count(nums[i]))
+            {
+                s.insert(nums[i]);
+                cnt++;
+                nums[i] = nums[nums[i]];
+            }
+            ans = max(ans, cnt);
+        }
+
+        return ans;
+    }
+};
+
+//  ----------------------------------------
+
+
+// -------------------------------  4ms submission  ---------------------------------------
+
+// class Solution {
+// public:
+//     int n;
+//     vector<int> visit;
+//     int arrayNesting(vector<int>& nums) {
+//         int ans = 0;
+//         n = nums.size();
+//         visit = vector<int>(n, 0);
+//         for (int i = 0; i < n; ++i) {
+//             if (visit[i] == 0) {
+//                 ans = max(ans, helper(nums, i));
+//             }
+//         }
+//         return ans;
+//     }
+//     int helper(vector<int>& nums, int i) {
+//         if (visit[i] == 1) return 0;
+//         visit[i] = 1;
+//         return 1 + helper(nums, nums[i]);
+//     }
+// };
+
+// ---------------------------------------------------------------------------------------------
