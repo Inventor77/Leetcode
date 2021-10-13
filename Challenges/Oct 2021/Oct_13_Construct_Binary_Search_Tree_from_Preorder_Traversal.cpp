dd86c4e8c68bd0dcc5859a20ee6a75b8443316bc
Shreedutt <shreedutt77@gmail.com>
2021-10-13 21:13:45 +0530

    day13 oct 2021

---
+++ Challenges/Oct 2021/Oct_13_Construct_Binary_Search_Tree_from_Preorder_Traversal.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder, int max_val = INT_MAX) {
+        if (i == preorder.size() || preorder[i] > max_val) return NULL;
+
+        TreeNode* root = new TreeNode(preorder[i++]);
+
+        root->left = bstFromPreorder(preorder, root->val);
+        root->right = bstFromPreorder(preorder, max_val);
+
+        return root;
+    }
+
+private:
+    int i = 0;
+};  
