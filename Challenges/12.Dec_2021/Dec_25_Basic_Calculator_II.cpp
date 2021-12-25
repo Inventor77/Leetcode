82029b809f218340c4b49d45069574ed94cfae71
Shreedutt <shreedutt77@gmail.com>
2021-12-25 08:40:53 +0530

    day25 dec 2021

---
+++ Challenges/12.Dec_2021/Dec_25_Basic_Calculator_II.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool digit(char c)
+    {
+        if (c >= '0' && c <= '9')
+            return 1;
+        return 0;
+    }
+    int calculate(string s) {
+        int n = s.length();
+        stack<int> st;
+        long long num = 0;
+        char op = '+';
+        for (int i = 0;i < n;i++)
+        {
+            char c = s[i];
+            if (digit(c))
+                num = num * 10 + c - '0';
+            if ((!digit(c) && c != ' ') || i == n - 1)
+            {
+                if (op == '+' || op == '-')
+                {
+                    if (op == '+')
+                        st.push(num);
+                    else
+                        st.push(-num);
+                }
+                else if (op == '*' || op == '/')
+                {
+                    int t = st.top();
+                    st.pop();
+                    if (op == '*')
+                        st.push(t * num);
+                    else
+                        st.push(t / num);
+                }
+                op = c;
+                num = 0;
+            }
+        }
+        int ans = 0;
+        while (!st.empty())
+        {
+            ans += st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
