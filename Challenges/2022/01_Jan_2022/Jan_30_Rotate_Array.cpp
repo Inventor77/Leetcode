795aecc93a877c996cf5da2d1698116c1a9e0fdc
Shreedutt <shreedutt77@gmail.com>
2022-01-30 20:01:39 +0530

    day30 jan 2022

---
+++ Challenges/2022/01_Jan_2022/Jan_30_Rotate_Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        int rows = gcd(n, k);
+        for (int i = 0; i < rows; i++) {
+            int temp = nums[i];
+            int curr_idx = i;
+            for (int j = 0; j < n / rows; j++) {
+                int next_idx = (curr_idx + k) % n;
+                int prev_temp = temp;
+                temp = nums[next_idx];
+                nums[next_idx] = prev_temp;
+                curr_idx = next_idx;
+            }
+        }
+    }
+};
