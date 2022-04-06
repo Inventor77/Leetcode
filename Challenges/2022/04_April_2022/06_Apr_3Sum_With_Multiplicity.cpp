b39e7b70670bd8eab81f7c2b7d5b7280203bacf4
Shreedutt <shreedutt77@gmail.com>
2022-04-07 00:11:37 +0530

    day6 apr 2022

--- Challenges/2022/04_April_2022/06_Apr_3Sum_With_Multiplicity.cpp
+++ Challenges/2022/04_April_2022/06_Apr_3Sum_With_Multiplicity.cpp
@@ -1,26 +1,21 @@
-#define M (1e9 + 7)
 class Solution {
 public:
 
-    int threeSumMulti(int* arr, int arrSize, int target)
+    int threeSumMulti(vector<int>& a, int t)
     {
-        unsigned long long c = 0;
-        unsigned long long hash[101] = { 0 };
-        for (int i = 0; i < arrSize; i++)
-            hash[arr[i]]++;
-        for (int i = 0; i <= 100; i++) {
-            for (int j = i; j <= 100; j++) {
-                int k = target - i - j;
-                if (k < 0 || k > 100)
-                    continue;
-                if (i < j && j < k)
-                    c += hash[i] * hash[j] * hash[k];
-                else if (i == j && j == k)
-                    c += hash[i] * (hash[i] - 1) * (hash[i] - 2) / 6;
-                else if (i == j && i != k)
-                    c += hash[i] * (hash[i] - 1) / 2 * hash[k];
-            }
+        int n = a.size();
+        unordered_map<int, long> mp;
+        int i, j, m = 1e9 + 7;
+        long c = 0;
+
+        for (i = 0;i < n;i++)
+        {
+            c += mp[t - a[i]];
+
+            for (j = 0;j < i;j++)
+                mp[a[i] + a[j]]++;
         }
-        return (int)(c % (int)M);
+
+        return c % m;
     }
 };
