f22d3793e6704182bdd58e059f1ab0f14e549577
Shreedutt <shreedutt77@gmail.com>
2022-07-08 09:11:16 +0530

    Time: 13 ms (34.23%), Space: 5.9 MB (40.88%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int rec(int i) {
+        if(i <= 1) return i;
+        return rec(i - 1) + rec(i - 2);
+    }
+    
+    int fib(int n) {
+        return rec(n);
+    }
+};
